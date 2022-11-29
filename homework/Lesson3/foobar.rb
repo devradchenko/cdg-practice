ab8e9ec11ba00e476b7bf1014e9cb2db9d92c3f3
devradchenko <dev.radchenko@yandex.ru>
2022-11-29 18:18:27 +0300

    add method foobar

---
+++ homework/Lesson3/foobar.rb
@@ -0,0 +1,16 @@
+puts "Введите первое число: "
+num1 = gets.to_i
+puts "Введите второе число: "
+num2 = gets.to_i
+
+def foobar(num1, num2)
+    if num1 == 20
+        puts num2
+    elsif num2 == 20
+        puts num1
+    else
+        puts num1 + num2
+    end
+end
+
+foobar(num1, num2)
